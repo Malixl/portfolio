3fe10afe474e30e4cfae60150f714c713bfd47b5
Malik <malikmatoha19052003@gmail.com>
2026-02-23 01:09:21 +0800

    feat: add @tailwindcss/typography dependency and include it in Tailwind CSS configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -64,6 +64,7 @@ export default {
         },
     },
     plugins: [
-        require("tailwindcss-animate")
+        require("tailwindcss-animate"),
+        require("@tailwindcss/typography"),
     ],
 }
